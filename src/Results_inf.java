d66fee7d0db8e0ee8755fc06299e8f065c36fee2
Salma Saeed <101990421+SalmaSaeed29@users.noreply.github.com>
2022-10-20 16:22:24 +0200

    Update Results_inf.java

--- src/Results_inf.java
+++ src/Results_inf.java
@@ -1,54 +1,51 @@
 import java.util.List;
 
 public class Results_inf {
-    //to calculate time
-    public Long time;
 
-    public Results_inf() {
+	private List<Node> path;
+	// to calculate time
+	public Long time;
 
-    }
+	public Results_inf() {
 
-    public Long getTime() {
-        return time;
-    }
+	}
 
-    public void setTime(Long time) {
-        this.time = time;
-    }
+	public Long getTime() {
+		return time;
+	}
 
-    //number of expanded nodes from intial state to goal state
-    public int nofNodesExpanded=0;
+	public void setTime(Long time) {
+		this.time = time;
+	}
 
-    public int getNofNodesExpanded() {
-        return nofNodesExpanded;
-    }
+	// number of expanded nodes from initial state to goal state
+	public int nofNodesExpanded = 0;
 
-    public void setNofNodesExpanded(int nofNodesExpanded) {
-        this.nofNodesExpanded = nofNodesExpanded;
-    }
+	public int getNofNodesExpanded() {
+		return nofNodesExpanded;
+	}
 
+	public void setNofNodesExpanded(int nofNodesExpanded) {
+		this.nofNodesExpanded = nofNodesExpanded;
+	}
 
-    public List<Node> path;
+	public List<Node> getPath() {
+		return path;
+	}
 
-    public List<Node> getPath() {
-        return path;
-    }
+	public void setPath(List<Node> path) {
+		this.path = path;
+	}
 
-    public void setPath(List<Node> path) {
-        this.path = path;
-    }
+	public int getCost() {
+		return nofNodesExpanded - 1;
+	}
 
-    public int getCost() {
-        return nofNodesExpanded- 1;
-    }
+	public Results_inf(Long time, int nofNodesExpanded, List<Node> path) {
+		this.nofNodesExpanded = nofNodesExpanded;
+		this.time = time;
+		this.path = path;
 
-
-    public Results_inf(Long time, int nofNodesExpanded , List<Node> path) {
-        this.nofNodesExpanded = nofNodesExpanded;
-        this.time = time;
-        this.path = path;
-
-    }
-
+	}
 
 }
